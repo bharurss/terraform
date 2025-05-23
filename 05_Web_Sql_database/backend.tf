19db6131a10ebc872a98e7dfe4773e6bfaf16e45
pbisht2017 <83567911+pbisht2017@users.noreply.github.com>
2025-05-23 10:57:19 +0530

    Update backend.tf

--- 05_Web_Sql_database/backend.tf
+++ 05_Web_Sql_database/backend.tf
@@ -1,7 +1,7 @@
 # terraform {
 #   backend "azurerm" {
 #     resource_group_name  = "tf_state"
-#     storage_account_name = "tfstate019"
+#     storage_account_name = "tfstate0198"
 #     container_name       = "tfstate"
 #     key                  = "terraform.tfstate"
 #   }
